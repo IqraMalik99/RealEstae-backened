aea3b3427734e79c085321c54d0fcde32361f100
IqraMalik99 <im4356927@gmail.com>
2025-06-13 16:43:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 dotenv.config()
 export const app = express();
 const corsOptions = {
-    origin: "https://real-estate-frontened-2j5k.vercel.app", // Replace with the actual origin of your frontend
+    origin: "https://real-estate-frontened-2j5k.vercel.app/", // Replace with the actual origin of your frontend
     credentials: true, // Allow credentials (cookies, headers)
 };
 
